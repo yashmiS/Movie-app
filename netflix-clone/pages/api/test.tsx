193695bfd48bc3f363c3515c2b3376a74b6f287a
yashmiS <savindiabeywickrama@gmail.com>
2024-02-27 22:50:10 +0530

    Add new pages

---
+++ netflix-clone/pages/api/test.tsx
@@ -0,0 +1,5 @@
+const MyPage = () => {
+  return <h1>Hi</h1>;
+};
+
+export default MyPage;
---
+++ netflix-clone/pages/auth.tsx
@@ -0,0 +1,6 @@
+const Auth = () => {
+  return (
+    <div className="relative h-full w-full bg-[url('/images/hero.jpeg')]"></div>
+  );
+};
+export default Auth;
