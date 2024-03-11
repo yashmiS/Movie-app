839180fe8511a14ccf568789e2daf1ea042f4687
yashmiS <savindiabeywickrama@gmail.com>
2024-03-11 22:56:49 +0530

    add interface

--- netflix-clone/components/input.tsx
+++ netflix-clone/components/input.tsx
@@ -1,4 +1,14 @@
-const Input = () => {
+import React from "react";
+
+interface InputProps {
+  id: string;
+  onChange: any;
+  value: string;
+  lable: string;
+  type?: string;
+}
+
+const Input: React.FC<InputProps> = () => {
   return (
     <div className="relative">
       <input
@@ -22,14 +32,26 @@ const Input = () => {
       />
       <label
         className="
-      absolute
-      text-md
+        absolute 
+        text-md
       text-zinc-400
-      duration-150
-      transform
-      -translate-y-3"
+        duration-150 
+        transform 
+        -translate-y-3 
+        scale-75 
+        top-4 
+        z-10 
+        origin-[0] 
+        left-6
+        peer-placeholder-shown:scale-100 
+        peer-placeholder-shown:translate-y-0 
+        peer-focus:scale-75
+        peer-focus:-translate-y-3
+      "
         htmlFor="email"
-      ></label>
+      >
+        Email
+      </label>
     </div>
   );
 };
